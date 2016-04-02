0fb1391931299be8bbe51e8e0e02c1d780cd6e8f
Monica <mengwuyoulin@126.com>
2016-04-03 02:14:55 +0800

    简单的表单，把验证逻辑移到表单中
    使用单元测试探索表单API
    换用Django中的ModelForm类
    测试和定制表单验证

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from lists.models import Item
 
-class ContactForm(froms.Form):
-	item_text = forms.CharField()
-	csrfmiddlewaretoken = forms.CharField(**widget=forms.hidden**)
+EMPTY_LIST_ERROR = "不能输入空事项"
+
+class ItemForm(forms.models.ModelForm):
+
+	class Meta:
+		model = Item
+		fields = ('text',)
+		widgets={
+			'text':forms.fields.TextInput(attrs={
+				'placeholder':'请输入任务名称',
+				'class':'form-control input-lg',
+			}),
+		}
+		error_messages = {
+			'text':{'required':EMPTY_LIST_ERROR}
+		}
