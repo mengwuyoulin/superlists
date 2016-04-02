24ee2b69435a6b4668c1705b7ee0f75186baa054
Monica <mengwuyoulin@126.com>
2016-04-02 17:59:01 +0800

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -25,6 +25,13 @@ class ListViewTest(TestCase):
 		list_ = List.objects.create()
 		response = self.client.get('/lists/%d/'%(list_.id,))
 		self.assertTemplateUsed(response,'list.html')
+
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.get('/lists/%d/'%(correct_list.id,))
+		self.assertEqual(response.context['list'],correct_list)
 		
 	def test_displays_only_item_for_that_list(self):
 		current_list = List.objects.create()
@@ -46,35 +53,39 @@ class ListViewTest(TestCase):
 		correct_list = List.objects.create()
 
 		response = self.client.post(
-			'/lists/%d/add_item'%(correct_list.id),
+			'/lists/%d/'%(correct_list.id),
 			data={'item_text':'A new item for an existing list'}
 		)
 
 		self.assertRedirects(response,'/lists/%d/'%(correct_list.id,))
 
-	def test_correct_list_to_template(self):
+	def test_can_save_a_POST_request_to_an_existing_list(self):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 
-		response = self.client.get('/lists/%d/'%(correct_list.id,))
-		self.assertEqual(response.context['list'],correct_list)
-
-class NewListTest(TestCase):
-	"""docstring for NewListTest"""
-
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		current_list = List.objects.create()
-
 		self.client.post(
-			'/lists/%d/add_item'%(current_list.id),
+			'/lists/%d/'%(correct_list.id),
 			data={'item_text':'A new item for an existing list'}
 		)
 
 		self.assertEqual(Item.objects.count(),1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text,'A new item for an existing list')
-		self.assertEqual(new_item.list,current_list)
+		self.assertEqual(new_item.list,correct_list)
+
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(
+			'/lists/%d/'%(correct_list.id,),
+			data={'item_text': 'A new list for an existing list'}
+		)
+		self.assertRedirects(response,'/lists/%d/'%(correct_list.id,))
+	
+
+class NewListTest(TestCase):
+	"""docstring for NewListTest"""
 
 	def test_saving_a_POST_request(self):
 		self.client.post('/lists/new',data={'item_text': 'A new list item'})
@@ -83,14 +94,7 @@ class NewListTest(TestCase):
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text,'A new list item')
 
-	def test_redirects_after_POST(self):
-		response = self.client.post(
-			'/lists/new',
-			data={'item_text': 'A new list item'}
-		)
-		new_list = List.objects.first()
-		self.assertRedirects(response,'/lists/%d/'%(new_list.id,))
-
+	
 	def test_validation_errors_are_sent_back_to_home_page_template(self):
 		response = self.client.post('/lists/new',data={'item_text':''})
 		self.assertEqual(response.status_code,200)
--- lists/views.py
+++ lists/views.py
@@ -12,6 +12,9 @@ def home_page(request):
 @csrf_exempt
 def view_list(request,list_id):
 	list_ = List.objects.get(id = list_id)
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'],list=list_)
+		return redirect('/lists/%d/'%(list_.id,))
 	return render(request,'list.html',{'list':list_})
 
 @csrf_exempt
@@ -19,12 +22,6 @@ def new_list(request):
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'],list=list_)
 	return redirect('/lists/%d/'%(list_.id,))
-
-@csrf_exempt
-def add_item(request,list_id):
-	list_ = List.objects.get(id = list_id)
-	Item.objects.create(text=request.POST['item_text'],list=list_)
-	return redirect('/lists/%d/' % (list_.id,))
 
 @csrf_exempt
 def new_list(request):
