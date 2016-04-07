3c9cfc4386632d3f1fff3b1720538d628feda3c2
Monica <mengwuyoulin@126.com>
2016-04-07 22:47:17 +0800

    move sqlite database outside of main source tree
    去掉点击编辑框隐藏错误提示消息
    将database移到主代码树外

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -60,19 +60,5 @@ class ItemValidationTest(FunctionalTest):
 		error = self.get_error_element()
 		self.assertFalse(error.is_displayed())
 
-	def test_error_messages_are_cleared_on_click(self):
-		#小明新建了一个清单，但方法不当，所以出现了一个验证错误
-		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('\n')
-		error = self.get_error_element()
-		self.assertTrue(error.is_displayed())
-
-		#为了消除错误，他在输入框点击了一下
-		self.get_item_input_box().click()
-
-		#他看到错误消失了，很高兴
-		error = self.get_error_element()
-		self.assertFalse(error.is_displayed())
-
 	def get_error_element(self):
 		return self.browser.find_element_by_css_selector('.has-error')
