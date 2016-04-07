389a6af526aa82cf849f4c58f853a6c61f746595
Monica <mengwuyoulin@126.com>
2016-04-07 21:49:07 +0800

    Revert "Revert "回到主线""
    This reverts commit 91def61263a4b5b7feff976addb1ace34f43e72b.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,13 +1,11 @@
 
+
 $('input').click(function(){//鼠标点击输入框就隐藏错误提示信息
-		alert('test');
-	});
+	$('.has-error').hide();
+});
 
 $(document).ready(function(){//确保执行脚本之前完全加载了页面
 	$('input').on('keypress',function(){//输入内容才才隐藏错误提示信息
 		$('.has-error').hide();
 	});
-	
-	
-
 });
